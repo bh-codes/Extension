9f636b887faf4770c1576d6a36a47724f3a80feb
bet_h <bettybhan@gmail.com>
2022-10-26 21:45:24 -0400

    Separated HTML and JS

---
+++ index.js
@@ -0,0 +1,26 @@
+const inputBtn = document.getElementById("input-btn")   
+const inputEl = document.getElementById("input-el")   
+const ulEl = document.getElementById("ul-el")
+
+let myLeads = []
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    renderLeads()
+    inputEl.value = ""
+})
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+                <a href="${myLeads[i]}" target="_blank">
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
