81c9d897eb224fd0ecf698aa36defb049e20b790
bet_h <bettybhan@gmail.com>
2022-10-31 22:30:27 -0400

    Updated event listeners

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
 const inputBtn = document.getElementById("input-btn")   
 const inputEl = document.getElementById("input-el")   
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 
 let myLeads = []
+let oldLeads = []
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    renderLeads()
-    inputEl.value = ""
-})
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads "))
 
-function renderLeads() {
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a href="${myLeads[i]}" target="_blank">
-                    ${myLeads[i]}
+                <a href="${leads[i]}" target="_blank">
+                    ${leads[i]}
                 </a>
             </li>
         `
     }
     ulEl.innerHTML = listItems
 }
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+    inputEl.value = ""
+})
+
+tabBtn.addEventListener("click", function() {
+    
+})
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
 
